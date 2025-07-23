ec010d2d39d5b86b5fbb3e2d3c671dc841c0973a
nukoseer <uygar.koseer@gmail.com>
2025-07-23 10:07:19 +0300

    Add which-key and new keys

--- lua/nu/init.lua
+++ lua/nu/init.lua
@@ -19,9 +19,29 @@ require("lazy").setup({
             "mbbill/undotree",
             branch = "master",
         },
+        {
+            "folke/which-key.nvim",
+            event = "VeryLazy",
+            opts = {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            },
+            keys = {
+                {
+                    "<leader>?",
+                    function()
+                        require("which-key").show({ global = false })
+                    end,
+                    desc = "Buffer Local Keymaps (which-key)",
+                },
+            },
+        }
     },
     checker = { enabled = true },
 })
+
+vim.g.netrw_banner = false
 
 vim.lsp.config('luals', {
   cmd = {'lua-language-server'},
--- lua/nu/remap.lua
+++ lua/nu/remap.lua
@@ -1,2 +1,6 @@
 print("remap hello")
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("i", "<C-c>", "<Esc>") 
+
+vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
